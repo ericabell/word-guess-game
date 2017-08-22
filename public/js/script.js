42d31c26b137f42fc8cc9853ef5d705292bb19f0
Eric Abell <ericabell@users.noreply.github.com>
2017-08-21 20:49:15 -0500

    Wrote the code for keypress sends post
    Now I don't need the text input or the submit button,
    but I will need an indication of what key the user pressed
    and whether that was a correct or incorrect guess.

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,31 @@
 console.log('Testing....');
+
+document.addEventListener('keypress', (e) => {
+  console.log(`User press: ${e.key}`);
+  // send the post request to / with letter="x" in the body
+  let XHR = new XMLHttpRequest();
+  let urlEncodedData = "";
+  let urlEncodedDataPairs = [];
+  let name;
+
+  urlEncodedDataPairs.push(encodeURIComponent('letter') + '=' + encodeURIComponent(e.key));
+
+  urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+
+  XHR.addEventListener('load', (e) => {
+    console.log('data sent and response loaded');
+  });
+
+  XHR.addEventListener('error', (e) => {
+    console.log('error in sending post' + e);
+  });
+
+  XHR.open('POST', 'http://localhost:3000/');
+
+  XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  XHR.send(urlEncodedData);
+
+  // refresh the page
+  window.location.reload();
+})
